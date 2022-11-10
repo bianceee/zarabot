61b6008723c44d32123f29b5b155cd6c745d6cae
Bianca <bianca.manueno@gmail.com>
2022-11-11 00:54:34 +0100

    category added, size object cleaned, price resolved

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import scraping from './nep.js';
+import bodyParser from 'body-parser'
 import plt from './plt.js';
 import { createClient } from '@supabase/supabase-js';
 import * as dotenv from 'dotenv';
@@ -12,35 +13,34 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 const app = express();
 const PORT = 3000;
 
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// app.post('/scraping', async (req, res) => {
+//     const data = await scraping('https://www.net-a-porter.com/fr-fr/shop/product/gianvito-rossi/chaussures/bottes/bottes-en-daim-hansen-70/38063312420989875');
+//     console.log(data);
+//     res.json(data)
+// })
 
 app.post('/scraping', async (req, res) => {
-    const data = await scraping('https://www.net-a-porter.com/fr-fr/shop/product/gianvito-rossi/chaussures/bottes/bottes-en-daim-hansen-70/38063312420989875');
-    console.log(data);
-    res.json(data)
-})
-
-app.get('/scraping/:uri', async (req, res) => {
-  const uri = req.params.uri;
-  const encoded = encodeURI(uri);
-  console.log(encoded);
-  console.log(uri);
-  const info = await plt(encoded);
+  const uri = req.body.uri;
+  const info = await plt(uri);
   const dataParsed = JSON.parse(info);
 
   // console.log(dataParsed);
 
+  const { name, price, sku, img, category } = dataParsed?.find(element => element.name);
 
-  const { name, price, sku, img } = dataParsed?.find(element => element.name);
+  // Todo: add shoe size choice programatically
+  // const { availability } = dataParsed?.find(element => element.size === '41');
 
-  const { availability } = dataParsed?.find(element => element.size === '41');
 
-
-  const sizes = dataParsed?.map(element => {
-    console.log("ELEMENT ", element);
+  const sizes = dataParsed?.filter(element => {
     if (element.size !== undefined) return { size: element.size }
     else return; 
-  }
-   );
+  });
 
   // console.log("SIZES ", sizes)
 
@@ -48,21 +48,29 @@ app.get('/scraping/:uri', async (req, res) => {
 
   const { data, error } = await supabase
   .from('products')
-  .insert({ name, price, availability, sku, img })
+  .insert({ 
+    name, 
+    price, 
+    availability: false, 
+    sku, 
+    img,
+    category
+   })
   .select();
 
 
-  // console.log('DATA ', data);
-  // console.log(error);
-
-  sizes.map(async(size) => {
-    const { datasize, errorsize } = await supabase
-    .from('sizes')
-    .insert({ size, product_id: data[0]?.id })
-    .select();
-    return datasize;
-  });
-
+  console.log('DATA ', data);
+  console.log(error);
+  
+  if (data) {
+    sizes.map(async(size) => {
+      const { datasize, errorsize } = await supabase
+      .from('sizes')
+      .insert({ size, product_id: data[0]?.id })
+      .select();
+      return datasize;
+    });
+  }
 
   res.json(data)
 })
--- plt.js
+++ plt.js
@@ -1,8 +1,8 @@
 import puppeteer from 'puppeteer';
 
 async function scraping(url) {
-    const browser = await puppeteer.launch({headless: true});
-
+  // const browser = await puppeteer.launch({headless: false, slowMo: 250, devtools: true }); // Pour debugger
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36');
   await page.goto(url, { waitUntil: "networkidle2" });
@@ -15,9 +15,18 @@ async function scraping(url) {
 
       const offers = document.querySelectorAll(".size-options .selproduct-size-ect .product-size-select");
       const name = document.querySelector(".product-view-title").innerText;
+      
 
-      const priceText = document.querySelector(".price-container.new .price .price").innerText;
-      const priceSplitted = priceText.split(' €')[0];
+      let basePrice;
+
+      // if there's no regular price then the base price is gonna be the new discounted price
+      if(!document.querySelector("#product-right .regular-price .price")) {
+        basePrice = document.querySelector(".price-container.new .price .price").innerHTML;
+      } else {
+        basePrice =  document.querySelector("#product-right .regular-price .price").innerHTML;
+      }
+
+      const priceSplitted = basePrice.split(' €')[0];
       const priceReplaced = priceSplitted.replace(',','.');
       const price = parseFloat(priceReplaced);
 
@@ -25,31 +34,37 @@ async function scraping(url) {
 
       const img = document.querySelector('.img-responsive').getAttribute('src');
 
-      // get all sizes
-      [...offers].forEach((offer) => {
-        const size = offer.innerText;
-        const availability = offer.classList.contains("size-is-out-of-stock");
+      const catName = document.querySelector(".breadcrumb li:nth-child(2) a").dataset.catname;
+      const category = catName.toLowerCase();
 
-        product.push({
-          size,
-          availability: !availability
+      // get all sizes
+      if(category !== "accessories") {
+        [...offers].forEach((offer) => {
+          const size = offer.innerText;
+          const availability = offer.classList.contains("size-is-out-of-stock");
+
+          product.push({
+            size,
+            availability: !availability
+          });
         });
-      });
+      }
 
       product.push({
         name,
         price,
         sku,
-        img
+        img,
+        category,
       });
 
       return product;
   }, bodyHandle);
 
-  // data.forEach((offer) => {
-  //   // console.table(offer)
-  //   console.log(`Taille ${offer.size} : ${offer.availability}`);
-  // });
+  data.forEach((offer) => {
+    console.table(offer)
+    console.log(`Taille ${offer.size} : ${offer.availability}`);
+  });
 
   browser.close();
   return JSON.stringify(data);
