a20d6b578d562e8e286b7c37d6932451eeb12311
Yann <lowscaa@gmail.com>
2022-11-24 16:10:36 +0100

    petit refacto

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import * as dotenv from 'dotenv'
-import Brand from './entities/Brand'
-import Product from './entities/Product'
-import Plt from './parsers/Plt'
+import scrap from "./scrapping/scrap";
 
 dotenv.config()
 
@@ -14,28 +12,11 @@ const PORT = 3000
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-// app.post('/scraping', async (req, res) => {
-//     const data = await scraping('https://www.net-a-porter.com/fr-fr/shop/product/gianvito-rossi/chaussures/bottes/bottes-en-daim-hansen-70/38063312420989875');
-//     console.log(data);
-//     res.json(data)
-// })
-
 app.post('/scraping', async (req, res) => {
-  const uri = req.body.uri
-  const info = await Plt(uri)
-  const dataParsed = JSON.parse(info)
+  console.log('Parsing : ' + req.body.uri)
+  await scrap(req.body.uri)
 
-  const regex = /(?<=https?:\/\/(?:www\.)?)(?!www\.).+?(?=\/)/
-  const brand = uri.match(regex)[0]
-  console.log(brand)
-
-  // Todo: add shoe size choice programatically
-  // const { availability } = dataParsed?.find(element => element.size === '41');
-
-  const newBrand = await Brand().findOrCreateBrand(brand)
-  const data = await Product(newBrand.uuid).findOrCreateProduct(dataParsed)
-
-  res.json(data)
+  res.json()
 })
 
 app.listen(PORT, () => {
