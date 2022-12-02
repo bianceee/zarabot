cd3198974ceae079e3ad33c1fc856e2622e5e6cf
Yann <lowscaa@gmail.com>
2022-12-02 14:37:52 +0100

    eslint: 'document' is not defined

--- mango.js
+++ mango.js
@@ -6,27 +6,27 @@ async function scraping(url) {
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36');
     await page.goto(url, { waitUntil: "networkidle2" });
-  
-  
+
+
     const bodyHandle = await page.$('body');
-  
+
     let data = await page.evaluate((body) => {
     	const product = [];
-			const offers = document.querySelectorAll('#sizesContainer li button span');
-      const price = document.querySelector('.product-features-prices [data-testid="currentPrice"] .text-title-xl');
-      const name = document.querySelector('.product-features .product-name');
-      const sku = document.querySelector('.product-reference');
+			const offers = body.querySelectorAll('#sizesContainer li button span');
+      const price = body.querySelector('.product-features-prices [data-testid="currentPrice"] .text-title-xl');
+      const name = body.querySelector('.product-features .product-name');
+      const sku = body.querySelector('.product-reference');
 
       product.push(offers, price, name, sku)
 
       return product;
-        
+
     }, bodyHandle);
 
-  
+
     browser.close();
     return JSON.stringify(data);
-  
+
   }
-  
+
   export default scraping;
